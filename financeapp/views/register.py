2f5a6c392ad95485f197bd847eec584424d2c89d
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2023-06-30 13:13:58 -0300

    Sistema de registro de usuários atualizado

--- financeapp/views/register.py
+++ financeapp/views/register.py
@@ -15,9 +15,37 @@ def register(request):
         password = request.POST.get('password')
         first_name = request.POST.get('name')
         last_name = request.POST.get('lastname')
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'financeapp/register.html', {
+                'error_message': 'Usuário já cadastrado.',
+                'username': username,
+                'email': email,
+                'first_name': first_name,
+                'last_name': last_name
+                })
         
+        if User.objects.filter(email=email).exists():
+            return render(request, 'financeapp/register.html', {
+                'error_message': 'E-mail já cadastrado.',
+                'username': username,
+                'email': email,
+                'first_name': first_name,
+                'last_name': last_name
+                })
+        
+        if len(password) < 8:
+            return render(request, 'financeapp/register.html', {
+                'error_message': 'A senha deve ter no mínimo 8 caracteres.',
+                'username': username,
+                'email': email,
+                'first_name': first_name,
+                'last_name': last_name
+                })
+
         user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
         user.save()
+        
         return redirect('/login')
     else:
         return render(request, 'financeapp/register.html')
